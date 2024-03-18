834e67a94b9764b655a8de5ba34e0767bb40fd92
CoolShivas <shivas2710cool00@gmail.com>
2024-03-18 17:16:20 +0530

    starting_of_restro

---
+++ my_restro_app/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/LayOut/Header";
+
+
+
+function App() {
+  return <>
+    <Header></Header>
+  </>
+}
+
+
+export default App;
---
+++ my_restro_app/src/components/LayOut/Header.jsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+    return <>
+        <header className="header">
+            <h1> Delicious Meals </h1>
+            <button> Cart </button>
+        </header>
+        <div className="main-image">
+            <img src="" alt="" />
+        </div>
+    </>
+}
+
+
+export default Header;
